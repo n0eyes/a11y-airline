29cbded299015caefbd4b286f798a74963cc22ee
n0eyes <doll4586633@naver.com>
2023-07-28 17:15:38 +0900

    feat: SpinButton 접근성 속성 추가

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,16 +1,16 @@
-import React, { useState, MouseEvent } from "react";
-import "./SpinButton.css";
+import React, { useState, MouseEvent } from 'react';
+import './SpinButton.css';
 
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
 
   const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+    if (count < 3) setCount((prevCount) => prevCount + 1);
   };
 
   const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
+    if (count > 0) setCount((prevCount) => prevCount - 1);
   };
 
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
@@ -22,29 +22,50 @@ const SpinButton: React.FC = () => {
       <div>
         <h1>승객 선택</h1>
         <div className="spinButtonLabel">
-          <label>성인</label>
+          <label id="adult-count-label" htmlFor="adult-count">
+            성인
+          </label>
           <div
             className="helpIcon"
             onMouseEnter={toggleTooltip}
-            onMouseLeave={toggleTooltip}
-          >
+            onMouseLeave={toggleTooltip}>
             ?
             {isTooltipVisible && (
               <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
             )}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button
+          onClick={decrement}
+          className="spinButton"
+          aria-label="성인 탑승자 한명 줄이기"
+          aria-live="assertive"
+          aria-relevant="additions"
+          aria-atomic="true"
+          aria-disabled={count === 0}>
           -
         </button>
         <input
+          id="adult-count"
           type="text"
           role="spinbutton"
           readOnly
           className="spinButtonInput"
           value={count}
+          aria-labelledby="adult-count-label"
+          aria-valuetext={`성인 승객 추가 ${count}`}
+          aria-valuemin={0}
+          aria-valuemax={3}
+          aria-live="assertive"
         />
-        <button onClick={increment} className="spinButton">
+        <button
+          onClick={increment}
+          className="spinButton"
+          aria-label="성인 탑승자 한명 늘리기"
+          aria-live="assertive"
+          aria-relevant="additions"
+          aria-atomic="true"
+          aria-disabled={count === 3}>
           +
         </button>
       </div>
